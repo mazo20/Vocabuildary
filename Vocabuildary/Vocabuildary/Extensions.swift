5a65a149ae76fe98e502a9a5357bb47a2dd41a49
Bartosz Olszanowski <bartosz.olszanowski@gmail.com>
2016-08-31 11:44:52 +0200

    Odstępy między funkcjami.

--- Vocabuildary/Vocabuildary/Extensions.swift
+++ Vocabuildary/Vocabuildary/Extensions.swift
@@ -11,6 +11,7 @@ import UIKit
 public func blueThemeColor() -> UIColor {
     return UIColor(red: 0, green: 0.6, blue: 1, alpha: 1)
 }
+// TIP: Pamiętaj o odstępach między funkcjami!
 public func timeFormatter(time: NSTimeInterval) -> String {
     let hours = time/3600
     let minutes = time%3600/60
@@ -24,24 +25,28 @@ public func timeFormatter(time: NSTimeInterval) -> String {
         return "\(Int(seconds))s"
     }
 }
+
 public func printD(date: NSDate) {
     let formatter = NSDateFormatter()
     formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZ"
     let defaultTimeZoneStr = formatter.stringFromDate(date)
     print(defaultTimeZoneStr)
 }
+
 public func stringFromDate(date: NSDate) -> String {
     let day = NSCalendar.currentCalendar().components(NSCalendarUnit.Day, fromDate: date).day
     let month = NSCalendar.currentCalendar().components(NSCalendarUnit.Month, fromDate: date).month
     let year = NSCalendar.currentCalendar().components(NSCalendarUnit.Year, fromDate: date).year
     return "\(year)-\(dateComponentFormatter(month))-\(dateComponentFormatter(day))"
 }
+
 public func dateComponentFormatter(component: Int) -> String {
     if component >= 10 {
         return "\(component)"
     }
     return "0\(component)"
 }
+
 extension NSDate {
     var today: NSDate {
         let date = NSDate()
