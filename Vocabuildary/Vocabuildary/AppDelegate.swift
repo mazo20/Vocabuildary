43d501033e3b55d52aa7d280b7ab9a8a74df12e6
Bartosz Olszanowski <bartosz.olszanowski@gmail.com>
2016-09-01 13:48:04 +0200

    Get rid of unused code.

--- Vocabuildary/Vocabuildary/AppDelegate.swift
+++ Vocabuildary/Vocabuildary/AppDelegate.swift
@@ -78,11 +78,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return succeeded
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
     func applicationDidEnterBackground(application: UIApplication) {
         let success = deckStore.saveChanges()
         if success {
@@ -114,7 +109,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.shortcutItem = nil
     }
 
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
 }
